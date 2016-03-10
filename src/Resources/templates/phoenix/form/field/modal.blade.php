6ffb8d3cf91f2d10c6e8381156b76635c70b5920
Simon Asika <asika32764@gmail.com>
2016-03-10 20:53:13 +0800

    Support modal disabled

--- src/Resources/templates/phoenix/form/field/modal.blade.php
+++ src/Resources/templates/phoenix/form/field/modal.blade.php
@@ -2,13 +2,14 @@
 
 <?php
 \Phoenix\Script\BootstrapScript::modal();
+$disabled = $attrs['readonly'] || $attrs['disabled'];
 ?>
 
 <div id="{{ $id }}-wrap">
     <div class="input-group">
         {!! $input !!}
         <span class="input-group-btn">
-            <a class="btn btn-info hasModal" type="button" href="{{ $url }}">
+            <a class="btn btn-info hasModal {{ $disabled ? 'disabled' : null }}" type="button" href="{{ $disabled ? 'javascript:void(0);' : $url }}">
                 @translate($field->getAttribute('buttonText', 'phoenix.form.field.modal.button.text'))
             </a>
         </span>
