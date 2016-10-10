7b863d312144f05d4863e8869405ace01df5e499
Simon Asika <asika32764@gmail.com>
2016-10-10 11:25:25 +0800

    Crypto Uuid #199

--- src/Resources/asset/js/phoenix/crypto.js
+++ src/Resources/asset/js/phoenix/crypto.js
@@ -75,9 +75,29 @@ var Phoenix;
             }).join("");
         },
 
+        /**
+         * Key char at.
+         *
+         * @param {string} key
+         * @param {Number} i
+         *
+         * @returns {Number}
+         */
         keyCharAt: function(key, i)
         {
             return key.charCodeAt( Math.floor(i % key.length) );
+        },
+
+        /**
+         * UUID v4
+         *
+         * @see  https://gist.github.com/jed/982883
+         *
+         * @returns {string}
+         */
+        uuid4: function()
+        {
+            return (function b(a) {return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)})();
         }
     };
 
