455a079e839af17e9091e81308bd45d7fd5977e2
Simon Asika <asika32764@gmail.com>
2017-05-14 12:30:50 +0800

    Support showon

---
+++ src/Resources/templates/phoenix/bootstrap/field/control.blade.php
@@ -0,0 +1,27 @@
+{{-- Part of phoenix project. --}}
+<?php
+/**
+ * @var $field   \Windwalker\Form\Field\AbstractField|\Windwalker\Form\Field\ListField
+ * @var $attribs array
+ */
+\Phoenix\Form\ShowonHelper::showon($field, $attribs, $field->get('showon'));
+
+?>
+
+@if (isset($attribs['transition']))
+    <transition name="{{ $attribs['transition'] }}">
+@endif
+
+    @php( isset($attribs['class']) ? $attribs['class'] = '' : null )
+    @php( $attribs['class'] .= ' form-group' )
+
+    <div {!! \Windwalker\Dom\Builder\HtmlBuilder::buildAttributes($attribs) !!}>
+        {!! $labelHtml !!}
+        <div class="{{ $field->get('fieldWidth', 'col-md-9') }} input-container">
+            {!! $inputHtml !!}
+        </div>
+    </div>
+
+@if (isset($attribs['transition']))
+    </transition>
+@endif
