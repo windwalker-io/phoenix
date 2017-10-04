b229d3ee555522a64d5b5d4d9c4f05d1f3e73e85
Simon Asika <asika32764@gmail.com>
2017-10-05 01:50:49 +0800

    Fix switch for IE11

--- src/Resources/templates/phoenix/bootstrap/field/switch.blade.php
+++ src/Resources/templates/phoenix/bootstrap/field/switch.blade.php
@@ -9,8 +9,8 @@ $vmodel = isset($attribs['v-model']) ? $attribs['v-model'] : false;
 $attribs['v-model'] = false;
 $color = $field->get('color', 'primary');
 ?>
-<label class="phoenix-switch">
-        <input name="{{ $field->getFieldName() }}" type="hidden" value="{{ $field->get('unchecked_value', 0) }}" />
+<label class="phoenix-switch" for="{{ $field->getId() }}">
+    <input name="{{ $field->getFieldName() }}" type="hidden" value="{{ $field->get('unchecked_value', 0) }}" />
     {!! $field->buildInput($attribs) !!}
     <span class="slider {{ $field->get('round') ? 'round' : '' }} {{ $color ? 'btn-' . $color : 'btn-default' }}"></span>
 </label>
