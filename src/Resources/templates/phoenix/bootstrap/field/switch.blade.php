e578cc2db2633f884b0c0e1fef618cfa7c9930b3
Simon Asika <asika32764@gmail.com>
2017-10-15 22:46:55 +0800

    Switch support disabled and vue

--- src/Resources/templates/phoenix/bootstrap/field/switch.blade.php
+++ src/Resources/templates/phoenix/bootstrap/field/switch.blade.php
@@ -3,14 +3,21 @@
 <?php
 /**
  * @var $field \Phoenix\Field\SwitchField
+ * @var $attribs array
  */
 
-$vmodel = isset($attribs['v-model']) ? $attribs['v-model'] : false;
-$attribs['v-model'] = false;
 $color = $field->get('color', 'primary');
+
+if ($field->attr('v-model'))
+{
+	$attribs[':true-value'] = $field->get('unchecked_value', 0);
+	$attribs[':false-value'] = $field->get('checked_value', 1);
+}
 ?>
 <label class="phoenix-switch" for="{{ $field->getId() }}">
-    <input name="{{ $field->getFieldName() }}" type="hidden" value="{{ $field->get('unchecked_value', 0) }}" />
+    <input id="{{ $attribs['id'] }}-unchecked" name="{{ $field->getFieldName() }}" type="hidden" value="{{ $field->get('unchecked_value', 0) }}"
+        {{ $field->get('disabled') ? 'disabled="disabled"' : '' }}
+        />
     {!! $field->buildInput($attribs) !!}
     <span class="switch-slider {{ $field->get('shape', 'slider-square') }} {{ $color ? 'btn-' . $color : 'btn-default' }}"></span>
 </label>
