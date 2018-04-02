f844dacd1a189fdacd4b5084503c1f29bee58fb9
Simon Asika <asika32764@gmail.com>
2018-04-02 23:18:41 +0800

    [Model Field] Remove delete button if is required #404

--- src/Resources/templates/phoenix/form/field/modal.blade.php
+++ src/Resources/templates/phoenix/form/field/modal.blade.php
@@ -6,7 +6,9 @@ $disabled = $attrs['readonly'] || $attrs['disabled'];
 ?>
 
 <div id="{{ $id }}-wrap">
+    @if (!$disabled)
     <div class="input-group">
+    @endif
         <input type="text"
                disabled="disabled"
                readonly="readonly"
@@ -15,18 +17,23 @@ $disabled = $attrs['readonly'] || $attrs['disabled'];
                value="{{ $title or '' }}"
                placeholder="{{ $attrs['placeholder'] }}"
         />
+        @if (!$disabled)
         <span class="input-group-btn input-group-append">
             <a class="btn btn-info hasModal {{ $disabled ? 'disabled' : null }}" role="button"
                href="{{ $disabled ? 'javascript:void(0);' : $url }}">
                 @translate($field->getAttribute('buttonText', 'phoenix.form.field.modal.button.text'))
             </a>
-            @if (!$disabled)
+
+            @if (!$attrs['required'])
                 <a href="javascript://" role="button" class="btn btn-default btn-info unselect-button"
-                   onclick="Phoenix.Field.Modal.select('#{{ $id }}-wrap', '', '')">
-                <span class="fa fa-remove fa-times"></span>
-            </a>
+                    onclick="Phoenix.Field.Modal.select('#{{ $id }}-wrap', '', '')">
+                    <span class="fa fa-remove fa-times"></span>
+                </a>
             @endif
         </span>
+        @endif
+    @if (!$disabled)
     </div>
+    @endif
     {!! $input !!}
 </div>
