9e620fd0c90bb0f4cf23a04de0f545595e55c206
Simon Asika <asika32764@gmail.com>
2019-03-31 20:30:50 +0800

    Add FilterWithoutStateTrait from Emooc #538

---
+++ src/Controller/Traits/FilterWithoutStateTrait.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Part of phoenix project.
+ *
+ * @copyright  Copyright (C) 2019 ${ORGANIZATION}.
+ * @license    __LICENSE__
+ */
+
+namespace Phoenix\Controller\Traits;
+
+use Windwalker\Filter\InputFilter;
+
+/**
+ * The FilterWithoutStateTrait class.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+trait FilterWithoutStateTrait
+{
+    /**
+     * Gets the value from session and input and sets it back to session
+     *
+     * @param string $name
+     * @param string $inputName
+     * @param mixed  $default
+     * @param string $filter
+     * @param string $namespace
+     *
+     * @return  mixed
+     */
+    public function getUserStateFromInput(
+        $name,
+        $inputName,
+        $default = null,
+        $filter = InputFilter::STRING,
+        $namespace = 'default'
+    ) {
+        return $this->input->get($inputName, $default, $filter);
+    }
+}
