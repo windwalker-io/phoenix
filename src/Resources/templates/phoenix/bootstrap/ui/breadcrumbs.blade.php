8d197402f85ebcc20a2c868a940d528b9cdde19a
Simon Asika <asika32764@gmail.com>
2019-04-28 17:31:27 +0800

    Breadcrumb cannot contains white spaces #550

--- src/Resources/templates/phoenix/bootstrap/ui/breadcrumbs.blade.php
+++ src/Resources/templates/phoenix/bootstrap/ui/breadcrumbs.blade.php
@@ -1,15 +1,9 @@
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb">
         @foreach ($paths as $i => $path)
-            <li class="breadcrumb-item {{ $path->active ? 'active' : '' }}" aria-current="page">
-                @if ($path->link)
-                    <a href="{{ $path->link }}">
-                        {{ $path->title }}
-                    </a>
-                @else
-                    {{ $path->title }}
-                @endif
-            </li>
+            <li class="breadcrumb-item {{ $path->active ? 'active' : '' }}"
+                aria-current="page"><a @attr('href', $path->link ?: null)
+                >{{ $path->title }}</a></li>
         @endforeach
     </ol>
 </nav>
