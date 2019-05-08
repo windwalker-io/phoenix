366e5582adf87e57ad3dbddbe948336ad4698598
Simon Asika <asika32764@gmail.com>
2019-05-09 01:55:26 +0800

    Use gray

--- src/Resources/templates/phoenix/grid/table/saveorder-button.blade.php
+++ src/Resources/templates/phoenix/grid/table/saveorder-button.blade.php
@@ -1,6 +1,6 @@
 {{-- Part of Phoenix project. --}}
 
-<button type="button" class="btn btn-default btn-outline-primary btn-sm btn-xs has-tooltip"
+<button type="button" class="btn btn-default btn-outline-secondary btn-sm btn-xs has-tooltip"
     onclick="{{ $phoenixJsObject }}.Grid.reorderAll();"
     title="@lang('phoenix.grid.ordering.saveorder')">
     <span class="fa fa-save"></span>
