b0461d121d5494ffa6f6624402d7cdf3d87a28cb
Simon Asika <asika32764@gmail.com>
2019-10-19 15:37:19 +0800

    Object.setPrototypeOf lake for phoenix.js #611

--- src/Resources/asset/js/phoenix/phoenix.js
+++ src/Resources/asset/js/phoenix/phoenix.js
@@ -345,6 +345,28 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
   window.Mixin = Mixin;
   window.mix = mix;
+})(); // @see https://juejin.im/post/5d887a9c518825094b34f41d
+
+
+(function () {
+  Object.setPrototypeOf = Object.setPrototypeOf || ({
+    __proto__: []
+  } instanceof Array ? setProtoOf : mixinProperties);
+
+  function setProtoOf(obj, proto) {
+    obj.__proto__ = proto;
+    return obj;
+  }
+
+  function mixinProperties(obj, proto) {
+    for (var prop in proto) {
+      if (!obj.hasOwnProperty(prop)) {
+        obj[prop] = proto[prop];
+      }
+    }
+
+    return obj;
+  }
 })();
 /**
  * Part of earth project.
